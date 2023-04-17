5c3c74e17dc4d5db527eca69df6ef5d49a3d8180
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-04-17 15:12:17 -0300

    04-map

--- 04-map/scripts.js
+++ 04-map/scripts.js
@@ -1,1 +1,15 @@
 // 4 map
+const products = [
+  { name: "Camiseta", price: 10.99, category: "Roupas" },
+  { name: "Chaleira Elétrica", price: 49.99, category: "Eletro" },
+  { name: "Fogão", price: 400.0, category: "Eletro" },
+  { name: "Calça Jeans", price: 50.99, category: "Roupas" },
+];
+
+products.map((product) => {
+  if (product.category === "Roupas") {
+    product.promocao = true;
+  }
+});
+
+console.log(products);
